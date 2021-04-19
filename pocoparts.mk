b06c55d179bfd9176757f1924c2f04d712ee7975
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-04-18 22:27:23 -0300

    Parts: add privapp-permissions
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- pocoparts.mk
+++ pocoparts.mk
@@ -8,6 +8,9 @@ PRODUCT_PACKAGES += \
     PocoParts \
     XiaomiDoze
 
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/PocoParts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
    $(VENDOR_PATH)/overlay

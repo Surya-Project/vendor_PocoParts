6652085acc9baa9087e08e5684f15c8ee269e255
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-06-02 10:23:08 -0300

    Parts: change all permissions and dependencies to PocoParts
    * Clean device tree too
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- pocoparts.mk
+++ pocoparts.mk
@@ -10,7 +10,10 @@ PRODUCT_PACKAGES += \
     XiaomiDirac
 
 PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/PocoParts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
+    $(VENDOR_PATH)/PocoParts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml \
+
+PRODUCT_COPY_FILES += \
+    vendor/PocoParts/init/init.pocoparts.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.pocoparts.rc
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \

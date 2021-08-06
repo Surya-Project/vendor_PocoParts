cdbe634a17572cf0fe3190f95ce8c7b7e581aa82
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-08-05 23:59:53 -0300

    Parts: rework PocoParts and update
    * we don't need all that dirac blobs
    
    * we don't need vendor.support too
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    
    Parts: remove all garbage unecessary
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    
    Revert "Parts:move Thermal Profiles for PocoParts (2/2)"
    
    This reverts commit 5d688d6.
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    
    Doze: move doze files for parts past
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    
    Parts: import Thermal Profiles from vayu
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    
    PocoParts: restart PocoParts
    * organize everything, including strings and feature order
    
    * all code was disorganized and confused, fix it
    
    * drop some unnecessary stuff
    
    * move some more stuff to the PocoParts menu
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    
    Initialize High Touch Polling Rate support
    * used to handle our node to force enable Xiaomi's High Touch polling rate mode from kernel
    
    * Commints necessary in kernel: https://github.com/Ramisky/kernel_xiaomi_sm8250/commit/9332a2907d9559c4d2fbb2b2859e658d94b61183
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- PocoParts/src/com/xiaomi/parts/BootReceiver.java
+++ PocoParts/src/com/xiaomi/parts/BootReceiver.java
@@ -27,7 +27,6 @@ import android.os.SELinux;
 import android.util.Log;
 import android.widget.Toast;
 import android.text.TextUtils;
-import com.xiaomi.parts.R;
 
 import com.xiaomi.parts.kcal.Utils;
 
@@ -36,67 +35,7 @@ import java.util.List;
 
 public class BootReceiver extends BroadcastReceiver implements Utils {
 
-    private static final String PREF_SELINUX_MODE = "selinux_mode";
-
-    private Context settingsContext = null;
-    private static final String TAG = "SettingsOnBoot";
-    private boolean mSetupRunning = false;
-    private Context mContext;
-
     public void onReceive(Context context, Intent intent) {
-
-    mContext = context;
-    ActivityManager activityManager =
-            (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-    List<ActivityManager.RunningAppProcessInfo> procInfos =
-            activityManager.getRunningAppProcesses();
-    for(int i = 0; i < procInfos.size(); i++) {
-        if(procInfos.get(i).processName.equals("com.google.android.setupwizard")) {
-            mSetupRunning = true;
-        }
-    }
-
-    if(!mSetupRunning) {
-        try {
-            settingsContext = context.createPackageContext("com.android.settings", 0);
-        } catch (Exception e) {
-            Log.e(TAG, "Package not found", e);
-        }
-        SharedPreferences sharedpreferences = context.getSharedPreferences("selinux_pref",
-                Context.MODE_PRIVATE);
-        if (sharedpreferences.contains(PREF_SELINUX_MODE)) {
-            boolean currentIsSelinuxEnforcing = SELinux.isSELinuxEnforced();
-            boolean isSelinuxEnforcing =
-                    sharedpreferences.getBoolean(PREF_SELINUX_MODE,
-                            currentIsSelinuxEnforcing);
-            if (isSelinuxEnforcing) {
-                if (!currentIsSelinuxEnforcing) {
-                    try {
-                        SuShell.runWithSuCheck("setenforce 1");
-                        showToast(context.getString(R.string.selinux_enforcing_toast_title),
-                                context);
-                    } catch (SuShell.SuDeniedException e) {
-                        showToast(context.getString(R.string.cannot_get_su), context);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else {
-                if (currentIsSelinuxEnforcing) {
-                    try {
-                        SuShell.runWithSuCheck("setenforce 0");
-                        showToast(context.getString(R.string.selinux_permissive_toast_title),
-                                context);
-                    } catch (SuShell.SuDeniedException e) {
-                        showToast(context.getString(R.string.cannot_get_su), context);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
-
         if (Settings.Secure.getInt(context.getContentResolver(), PREF_ENABLED, 0) == 1) {
             FileUtils.setValue(KCAL_ENABLE, Settings.Secure.getInt(context.getContentResolver(),
                     PREF_ENABLED, 0));
@@ -123,8 +62,6 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
                     PREF_HUE, HUE_DEFAULT));
         }
 
-        FileUtils.setValue(DeviceSettings.USB_FASTCHARGE_PATH, Settings.Secure.getInt(context.getContentResolver(),
-                DeviceSettings.PREF_USB_FASTCHARGE, 0));
     }
 
     private void showToast(String toastString, Context context) {
--- PocoParts/src/com/xiaomi/parts/DeviceSettings.java
+++ PocoParts/src/com/xiaomi/parts/DeviceSettings.java
@@ -35,8 +35,6 @@ import com.xiaomi.parts.speaker.ClearSpeakerActivity;
 import com.xiaomi.parts.preferences.CustomSeekBarPreference;
 import com.xiaomi.parts.preferences.SecureSettingListPreference;
 import com.xiaomi.parts.preferences.SecureSettingSwitchPreference;
-import com.xiaomi.parts.SuShell;
-import com.xiaomi.parts.SuTask;
 
 public class DeviceSettings extends PreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -46,20 +44,12 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String CATEGORY_DISPLAY = "display";
     public static final String PREF_DEVICE_KCAL = "device_kcal";
 
-    public static final String CATEGORY_FASTCHARGE = "usb_fastcharge";
-    public static final String PREF_USB_FASTCHARGE = "fastcharge";
-    public static final String USB_FASTCHARGE_PATH = "/sys/kernel/fast_charge/force_fast_charge";
     public static final String PREF_KEY_FPS_INFO = "fps_info";
-    private static final String SELINUX_CATEGORY = "selinux";
-    private static final String PREF_SELINUX_MODE = "selinux_mode";
-    private static final String PREF_SELINUX_PERSISTENCE = "selinux_persistence";
 
     private static final String PREF_CLEAR_SPEAKER = "clear_speaker_settings";
 
     private Preference mKcal;
     private SecureSettingSwitchPreference mFastcharge;
-    private SwitchPreference mSelinuxMode;
-    private SwitchPreference mSelinuxPersistence;
     private Preference mClearSpeakerPref;
     private Preference mAmbientPref;
 
@@ -90,49 +80,16 @@ public class DeviceSettings extends PreferenceFragment implements
             return true;
         });
 
-        if (FileUtils.fileWritable(USB_FASTCHARGE_PATH)) {
-            mFastcharge = (SecureSettingSwitchPreference) findPreference(PREF_USB_FASTCHARGE);
-            mFastcharge.setEnabled(Fastcharge.isSupported());
-            mFastcharge.setChecked(Fastcharge.isCurrentlyEnabled(this.getContext()));
-            mFastcharge.setOnPreferenceChangeListener(new Fastcharge(getContext()));
-        } else {
-            getPreferenceScreen().removePreference(findPreference(CATEGORY_FASTCHARGE));
-        }
-
         SwitchPreference fpsInfo = (SwitchPreference) findPreference(PREF_KEY_FPS_INFO);
         fpsInfo.setChecked(prefs.getBoolean(PREF_KEY_FPS_INFO, false));
         fpsInfo.setOnPreferenceChangeListener(this);
 
-        // SELinux
-        Preference selinuxCategory = findPreference(SELINUX_CATEGORY);
-        mSelinuxMode = (SwitchPreference) findPreference(PREF_SELINUX_MODE);
-        mSelinuxMode.setChecked(SELinux.isSELinuxEnforced());
-        mSelinuxMode.setOnPreferenceChangeListener(this);
-
-        mSelinuxPersistence =
-        (SwitchPreference) findPreference(PREF_SELINUX_PERSISTENCE);
-        mSelinuxPersistence.setOnPreferenceChangeListener(this);
-        mSelinuxPersistence.setChecked(getContext()
-        .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE)
-        .contains(PREF_SELINUX_MODE));
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object value) {
         final String key = preference.getKey();
         switch (key) {
-            case PREF_SELINUX_MODE:
-                  if (preference == mSelinuxMode) {
-                  boolean enabled = (Boolean) value;
-                  new SwitchSelinuxTask(getActivity()).execute(enabled);
-                  setSelinuxEnabled(enabled, mSelinuxPersistence.isChecked());
-                  return true;
-                } else if (preference == mSelinuxPersistence) {
-                  setSelinuxEnabled(mSelinuxMode.isChecked(), (Boolean) value);
-                  return true;
-                }
-                break;
-
             case PREF_KEY_FPS_INFO:
                 boolean enabled = (Boolean) value;
                 Intent fpsinfo = new Intent(this.getContext(), com.xiaomi.parts.FPSInfoService.class);
@@ -148,45 +105,6 @@ public class DeviceSettings extends PreferenceFragment implements
         }
         return true;
     }
-
-        private void setSelinuxEnabled(boolean status, boolean persistent) {
-          SharedPreferences.Editor editor = getContext()
-              .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE).edit();
-          if (persistent) {
-            editor.putBoolean(PREF_SELINUX_MODE, status);
-          } else {
-            editor.remove(PREF_SELINUX_MODE);
-          }
-          editor.apply();
-          mSelinuxMode.setChecked(status);
-        }
-
-        private class SwitchSelinuxTask extends SuTask<Boolean> {
-          public SwitchSelinuxTask(Context context) {
-            super(context);
-          }
-          @Override
-          protected void sudoInBackground(Boolean... params) throws SuShell.SuDeniedException {
-            if (params.length != 1) {
-              Log.e(TAG, "SwitchSelinuxTask: invalid params count");
-              return;
-            }
-            if (params[0]) {
-              SuShell.runWithSuCheck("setenforce 1");
-            } else {
-              SuShell.runWithSuCheck("setenforce 0");
-            }
-          }
-
-          @Override
-          protected void onPostExecute(Boolean result) {
-            super.onPostExecute(result);
-            if (!result) {
-              // Did not work, so restore actual value
-              setSelinuxEnabled(SELinux.isSELinuxEnforced(), mSelinuxPersistence.isChecked());
-            }
-          }
-        }
 
     private boolean isAppNotInstalled(String uri) {
         PackageManager packageManager = getContext().getPackageManager();
